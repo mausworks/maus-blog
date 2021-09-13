6bae430c8e6670b3089059d4277790d6adb51784
mausworks <rasmuswennerstrom@gmail.com>
2021-09-13 03:24:50 +0200

    feat: add custom document with fonts

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Archivo:ital,wght@0,400;0,500;0,800;1,400&family=Source+Code+Pro:wght@500&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.png" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
