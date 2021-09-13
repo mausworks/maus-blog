855bfe37a88a02b88e314994ef90fb5df6bea1a7
mausworks <rasmuswennerstrom@gmail.com>
2021-09-13 18:34:34 +0200

    fix: viewport and meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,17 +9,13 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin=""
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,800;1,400&family=Archivo+Narrow&family=Source+Code+Pro:wght@500&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" href="/favicon.png" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1"
-          ></meta>
         </Head>
 
         <body>
