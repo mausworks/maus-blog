5875273fe1f1d21b8a1a94c843e5eb00c270a64b
mausworks <rasmuswennerstrom@gmail.com>
2021-09-13 11:32:10 +0200

    fix: fonts and colors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin=""
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&family=Archivo:ital,wght@0,400;0,500;0,800;1,400&family=Source+Code+Pro:wght@500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,800;1,400&family=Archivo+Narrow&family=Source+Code+Pro:wght@500&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" href="/favicon.png" />
