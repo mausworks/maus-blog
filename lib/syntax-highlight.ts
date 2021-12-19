aed9259acd264d61905d745d5ea9080059257b3a
mausworks <rasmuswennerstrom@gmail.com>
2021-12-19 23:57:45 +0100

    feat: support highlighting ini-files

--- lib/syntax-highlight.ts
+++ lib/syntax-highlight.ts
@@ -3,7 +3,7 @@ import theme from "./mausworks-night-theme.json";
 
 const options: shiki.HighlighterOptions = {
   theme: theme as any,
-  langs: ["ts", "tsx", "jsx", "js", "sh", "html", "css"],
+  langs: ["ts", "tsx", "jsx", "js", "sh", "html", "css", "ini"],
 };
 
 let highlighter: shiki.Highlighter | null = null;
